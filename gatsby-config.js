54437fafb5828517621e5b219b3cb787b79e50cb
Mohd Danish <mddanishyusuf@gmail.com>
2022-06-19 22:07:07 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
